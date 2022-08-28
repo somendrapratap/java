66dd76514a93cd448460b9645ef89f8c07ead4e1
Somendra Pratap Singh <111568537+somendra-singh@users.noreply.github.com>
2022-08-28 19:23:14 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,7 @@
+class Main {
+  public static void main(String[] args) {
+    TwoSum t = new TwoSum();
+    t.callTwoSum();
+    System.out.println("Hello world!");
+  }
+}
---
+++ leetcode/arrays/easy/twosum/TwoSum.java
@@ -0,0 +1,19 @@
+class TwoSum{
+  class Solution{
+    public void test(){
+          System.out.println("Called Test");
+    }
+  }
+
+  // class Solution{
+  //   public void test(){
+  //         System.out.println("Called Test");
+  //   }
+  // }
+
+  
+  public void callTwoSum(){
+    new Solution().test();
+    System.out.println("Called");
+  }
+}
