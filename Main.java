2f611d51418977c57321cb9c5256dd0036b0081a
Somendra Pratap Singh <111568537+somendra-singh@users.noreply.github.com>
2022-08-28 19:29:07 +0000

    Added Strings

--- Main.java
+++ Main.java
@@ -1,7 +1,17 @@
 class Main {
   public static void main(String[] args) {
+
+    // region Arrays
+    
     TwoSum t = new TwoSum();
     t.callTwoSum();
     System.out.println("Hello world!");
+
+    // endregion
+
+    // region Strings
+    AddStrings a = new AddStrings();
+    a.test();
+    // endregion
   }
 }
---
+++ leetcode/strings/easy/addstrings/AddStrings.java
@@ -0,0 +1,6 @@
+class AddStrings{
+  public void test(){
+  System.out.println("Add Strings");  
+  }
+  
+}
